2532bfa5c9befd94068d0ed9b93334463ac9c463
Scott Frechette <scott.frechette@regions.com>
2019-12-06 13:40:25 -0600

    Add convert_ratio function

--- R/remove_bigram_stopwords.R
+++ R/remove_bigram_stopwords.R
@@ -3,17 +3,27 @@
 #' @name remove_bigram_stopwords
 #' @param df A tibble containing bigrams
 #' @param bigram Column of bigrams
+#' @param char_only Remove numbers
 #' @export
 
-remove_bigram_stopwords <- function(df, bigrams) {
+remove_bigram_stopwords <- function(df, bigrams, char_only = TRUE) {
 
-  df %>%
+  df <- df %>%
     tidyr::separate({{bigrams}}, c("word1", "word2"),
                     sep = " ", extra = "drop", fill = "right") %>%
     dplyr::filter(!word1 %in% stop_words$word,
                   !word2 %in% stop_words$word,
                   !is.na(word2)) %>%
     tidyr::unite({{bigrams}}, word1, word2, sep = " ")
+
+  if(char_only) {
+
+    df <- df %>%
+      dplyr::filter(!stringr::str_detect({{bigrams}}, "\\d"))
+
+  }
+
+  df
 
 }
 
